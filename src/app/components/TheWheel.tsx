6814ed884a0ffaabf15af79bf9397d531a6033d3
John Barry <john.olivierb@gmail.com>
2024-01-19 01:01:58 -0500

    Test img

--- src/app/components/TheWheel.tsx
+++ src/app/components/TheWheel.tsx
@@ -6,7 +6,6 @@ import { Wheel } from 'react-custom-roulette'
 import WheelLabel from './WheelLabel';
 import { Button } from "@/app/components/ui/button"
 
-
 const data = CHOICES
 
 const RandomWheel = () => {
@@ -26,35 +25,30 @@ const RandomWheel = () => {
   return (
     <div className='flex'>
       <div className='flex flex-col gap-3'>
-      <WheelLabel color="#393939" label='Skin mystere' />
-      <WheelLabel color="#DFA718" label='Skin mystere +950' />
-      <WheelLabel color="#2545ED" label='Skin legendaire' />
-      <WheelLabel color="#5200FF" label='Skin ultimate' />
-      <WheelLabel color="#FF2459" label='Commentateur décide' />
-     </div>
+        <WheelLabel color="#393939" label='Skin mystere' />
+        <WheelLabel color="#DFA718" label='Skin mystere +950' />
+        <WheelLabel color="#2545ED" label='Skin legendaire' />
+        <WheelLabel color="#5200FF" label='Skin ultimate' />
+        <WheelLabel color="#FF2459" label='Commentateur décide' />
+      </div>
 
       <div className='flex flex-col'>
-      <Wheel
-        mustStartSpinning={mustSpin}
-        prizeNumber={prizeNumber}
-        data={data}
-        perpendicularText={true}
-        radiusLineWidth={1}
-        outerBorderWidth={1}
-        outerBorderColor="#FFFFFF"
-        innerBorderColor="#FFFFFF"
-        radiusLineColor='#FFFFFF'
-        backgroundColors={['#0F0F0F']}
-        textColors={['#FFFFFF']}
-        textDistance={70}
-        fontSize={15}
-        onStopSpinning={() => {
-          setMustSpin(false);
-        }}
+        <Wheel
+          mustStartSpinning={mustSpin}
+          prizeNumber={prizeNumber}
+          data={data}
+          perpendicularText={true}
+          radiusLineWidth={0}
+          textColors={['#FFFFFF']}
+          textDistance={70}
+          fontSize={15}
+          onStopSpinning={() => {
+            setMustSpin(false);
+          }}
         />
-        
+
         <Button variant="outline" onClick={handleSpinClick}>GO!</Button>
-    </div>
+      </div>
     </div>
   );
 };
--- src/app/data/constants.ts
+++ src/app/data/constants.ts
@@ -1,5 +1,5 @@
 export const CHOICES = [
-  { option: '', optionSize: 44, style: { backgroundColor: '#393939' } },
+  { option: '', optionSize: 44, style: { backgroundColor: '#393939' }, image: { uri: 'https://dummyimage.com/600x400/000/fff'} },
   { option: '', optionSize: 23, style: { backgroundColor: '#DFA718' } },
   { option: '', optionSize: 15, style: { backgroundColor: '#2545ED' } },
   { option: '', optionSize: 10, style: { backgroundColor: '#5200FF' } },
