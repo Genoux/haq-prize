13aeff5308584c7fe2acd0e61d3292ff95387d4f
John Barry <john.olivierb@gmail.com>
2024-01-20 00:47:26 -0500

    Choix des commentateurs

--- src/app/data/constants.ts
+++ src/app/data/constants.ts
@@ -3,5 +3,5 @@ export const CHOICES = [
   { name:'Skin Mystère +950', option: '', optionSize: 23, style: { backgroundColor: '#DFA718' } },
   { name:'Skin Légendaire', option: '', optionSize: 15, style: { backgroundColor: '#2545ED' } },
   { name:'Skin ULTIMATE', option: '', optionSize: 10, style: { backgroundColor: '#5200FF' } },
-  { name:'Commentateur décide', option: '', optionSize: 8, style: { backgroundColor: '#FF2459' } },
+  { name:'Choix des commentateurs', option: '', optionSize: 8, style: { backgroundColor: '#FF2459' } },
 ];
