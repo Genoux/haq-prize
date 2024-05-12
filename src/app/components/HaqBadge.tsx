ad05f6c100f84b0b227e6cc2ce2c184f492c6725
John Barry <john.olivierb@gmail.com>
2024-05-12 14:47:57 -0400

    Update package for DAM

--- src/app/components/HaqBadge.tsx
+++ src/app/components/HaqBadge.tsx
@@ -1,8 +1,11 @@
+import { Logo } from 'haq-assets'
+
+
 export default function HaqBadge() {
-    return (
-      <div className="w-fit flex items-center rounded-full bg-muted px-3 py-1 text-sm font-medium gap-2"><svg width="12" height="17" viewBox="0 0 12 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M11.8924 7.76912C11.7104 7.02042 11.2856 6.35166 10.5512 6.01882C10.3891 5.94579 10.0634 5.89199 9.84904 5.86431C9.71691 5.84663 9.60245 5.76362 9.54484 5.64447L6.83156 0C6.83156 0 3.54599 11.5941 0.7582 5.65293C0.7582 5.65293 -1.21223 9.3211 1.19991 10.757C1.36123 10.8531 1.73535 10.9915 2.22622 11.0291C3.41155 8.01279 5.97349 5.86201 8.96408 5.95656C8.76588 6.01498 8.57307 6.08877 8.38332 6.16948C6.37602 7.02273 4.88879 8.77456 4.40022 10.923C3.92855 12.9954 4.49548 15.23 5.7607 16.918C4.96485 14.2338 8.27654 6.22867 11.3754 11.033C11.3754 11.033 11.8517 9.7923 11.9001 9.57861C12.0292 9.00978 12.0392 8.36332 11.8947 7.76835H11.8932L11.8924 7.76912Z" fill="white" />
-    </svg>
-      Tournois HAQ</div>
-    );
+  return (
+    <div className="w-fit flex items-center rounded-full bg-black pl-2 pr-3 py-1 text-sm font-bold gap-1">
+      <Logo size={18} />
+      Tournois HAQ
+    </div>
+  );
 }
